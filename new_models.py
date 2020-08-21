d57ce90eb02862e887029363d2fe27c8211caa4c
yihengy <yihengy@uchicago.edu>
2020-08-21 16:32:11 +0800

    fix

---
+++ new_models.py
@@ -0,0 +1,162 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as f
+from torch.autograd import Variable
+
+'''
+Given a batch of images and a HUGE tensor produced by the network,
+compute the output batch of images
+'''
+
+# reshape an N*input_channel*x*y tensor into an N*input_channel*x*y*(k*k) tensor
+def reshape(data,ker_size=5):
+    pad = (ker_size-1)//2
+    ZeroPad = nn.ZeroPad2d(padding=(pad, pad, pad, pad))
+    data_pad = ZeroPad(data)
+    
+    N = (list(data.size()))[0]
+    in_ch = (list(data.size()))[1]
+    x = (list(data.size()))[2]
+    y = (list(data.size()))[3]
+    
+    reshape_data = torch.zeros(N, in_ch, x, y, in_ch*ker_size**2)
+    
+    for i in range(N):
+        for j in range(in_ch):
+            for k in range(x):
+                for l in range(y):
+                    m = 0
+                    for n in range(ker_size):
+                        for o in range(ker_size):
+                            reshape_data[i][j][k][l][m] = data_pad[i][j][k+n][l+o]
+                            m += 1
+    return reshape_data
+    
+def reshape_output(kernel, ker_size=5):
+    N = (list(kernel.size()))[0]
+    in_ch = (list(kernel.size()))[1] // (ker_size**2)
+    x = (list(kernel.size()))[2]
+    y = (list(kernel.size()))[3]
+    o_ch = (list(kernel.size()))[1]
+    
+    reshape_kernel = torch.zeros(N, in_ch, x, y, o_ch)
+    for i in range(N):
+        for j in range(in_ch):
+            for k in range(x):
+                for l in range(y):
+                    for m in range(o_ch):
+                        reshape_kernel[i][j][k][l][m] = kernel[i][in_ch*j+m][k][l]
+    # tested so far
+    
+    # Normalization of reshape_kernel = torch.zeros(N, in_ch, x, y, o_ch)
+    for a in range(N):
+        for b in range(in_ch):
+            for c in range(x):
+                for d in range(y):
+                    sum = 0
+                    for e in range(o_ch):
+                        sum += reshape_kernel[a][b][c][d][e]
+                        reshape_kernel[a][b][c][d] /= sum
+    return reshape_kernel
+
+
+def compute_output(data, kernel, ker_size=5):
+    # data = N*in_channel*x*y
+    # kernel = N*out_channel*x*y, out_channel=in_ch*k**2
+    # transform kernel into N*input_channel*x*y*(k*k), and then do a simple matrix scalar product
+    reshape_data = reshape(data, ker_size)
+    reshape_kernel = reshape_output(kernel, ker_size)
+    
+    N = (list(data.size()))[0]
+    in_ch = (list(data.size()))[1]
+    x = (list(data.size()))[2]
+    y = (list(data.size()))[3]
+    o_ch = (list(kernel.size()))[1] // in_ch
+    scalar_product = torch.mul(reshape_kernel, reshape_data)
+    
+    result = torch.zeros(N, in_ch, x, y)
+    
+    for o in range(N):
+        for p in range(in_ch):
+            for q in range(x):
+                for r in range(y):
+                    sum = 0
+                    for s in range(o_ch):
+                        sum += scalar_product[o][p][q][r][s]
+                    result[o][p][q][r] = sum
+    #print(result.size())
+    return result
+    
+class DnCNN(nn.Module):
+    def __init__(self, channels=1, num_of_layers=9, ker_size=3, o_k_size=5):
+        super(DnCNN, self).__init__()
+        padding = (ker_size-1)//2
+        o_channels = o_k_size**2
+        features = 150
+        
+        layers = []
+        
+        layers.append(nn.Conv2d(in_channels=channels, out_channels=features, kernel_size=ker_size, padding=padding, bias=False))
+        layers.append(nn.ReLU(inplace=True))
+        
+        for _ in range(num_of_layers-2):
+            layers.append(nn.Conv2d(in_channels=features, out_channels=features, kernel_size=ker_size, padding=padding, bias=False))
+            layers.append(nn.ReLU(inplace=True))
+            
+        layers.append(nn.Conv2d(in_channels=features, out_channels=o_channels, kernel_size=ker_size, padding=padding, bias=False))
+        
+        self.dncnn = nn.Sequential(*layers)
+        
+    def forward(self, data, o_k_size=5):
+        o_kernel = self.dncnn(data)
+        return o_kernel
+
+class PatchLoss(nn.Module):
+    def __initII(self, size_average=None, reduce=None, reduction: str = 'mean') -> None:
+        super(PatchLoss, self).__init__(size_average, reduce, reduction)
+
+    def forward(self, kernel, noisy, target, o_ker_size, patch_size):
+        output = compute_output(noisy, kernel, o_ker_size).squeeze(1)
+        avg_loss = 0
+        for i in range(len(output)):
+            output_patches = output[i].unfold(0, patch_size, patch_size).unfold(1, patch_size, patch_size)
+            target_patches = target[i].unfold(0, patch_size, patch_size).unfold(1, patch_size, patch_size)
+            max_patch_loss = 0
+            for j in range(list(output_patches.size())[0]):
+                for k in range(list(output_patches.size())[1]):
+                    max_patch_loss = max(max_patch_loss, f.l1_loss(output_patches[j][k], target_patches[j][k]))
+            avg_loss+=max_patch_loss
+        avg_loss/=len(output)
+        return avg_loss
+
+
+if __name__=="__main__":
+    criterion = PatchLoss()
+    dtype = torch.FloatTensor
+    
+    x = Variable(torch.randn(100, 100).type(dtype), requires_grad=False)
+    y = Variable(torch.randn(100, 100).type(dtype), requires_grad=False)
+    loss = criterion(x, y, 10)
+    
+    input = torch.randn(10, 25, 16, 16)
+    reshape_output(input, 5)
+    
+    data = torch.randn(10, 1, 16, 16)
+    reshape(data,5)
+    
+    t = compute_output(data, input, 5)
+    print(t.size())
+    
+    net = DnCNN()
+    shit = torch.randn(10, 1, 8, 8)
+    shithole = net(shit, o_k_size=5)
+    print(shithole.size())
+    
+    
+'''
+NOTE
+Example: input: torch.Size([10, 1, 16, 16]), batchsize=10, 1 channel, image size 16*16
+output: torch.Size([10, 9, 16, 16])
+'''
+    
+
---
+++ new_train.py
@@ -0,0 +1,155 @@
+# modified from github.com/SaoYan/DnCNN-PyTorch/blob/master/train.py
+import os
+import argparse
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+from torch.nn import functional as f
+from models import DnCNN, PatchLoss
+from dataset import *
+import glob
+import torch.optim as optim
+import uproot
+import matplotlib.pyplot as plt
+from torch.utils.data import DataLoader
+import math
+
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+
+# parse arguments
+parser = argparse.ArgumentParser(description="DnCNN")
+parser.add_argument("--num_of_layers", type=int, default=9, help="Number of total layers")
+parser.add_argument("--sigma", type=float, default=20, help='noise level')
+parser.add_argument("--outf", type=str, default="logs/kp_820", help='path of log files')
+parser.add_argument("--epochs", type=int, default=1, help="Number of training epochs")
+parser.add_argument("--lr", type=float, default=1e-3, help="Initial learning rate")
+parser.add_argument("--trainfile", type=str, default="./data/training/part1.root", help='path of .root file for training')
+parser.add_argument("--valfile", type=str, default="./data/validation/part2.root", help='path of .root file for validation')
+parser.add_argument("--batchSize", type=int, default=100, help="Training batch size")
+parser.add_argument("--model", type=str, default=None, help="Existing model, if applicable")
+parser.add_argument("--patchSize", type=int, default=20, help="Size of patches to apply in loss function")
+parser.add_argument("--kernelSize", type=int, default=3, help="Training kernel size")
+parser.add_argument("--outKerSize", type=int, default=5, help="Output kernel size")
+args = parser.parse_args()
+
+def init_weights(m):
+    if type(m) == nn.Linear:
+        torch.nn.init.xavier_uniform(m.weight)
+        m.bias.data.fill_(0.01)
+
+def main():
+
+    # creat_readme()
+    # choose cpu or gpu
+    if torch.cuda.is_available():
+        args.device = torch.device('cuda')
+    else:
+        args.device = torch.device('cpu')
+
+    print('Loading Dataset--')
+    dataset_train = RootDataset(root_file=args.trainfile, sigma=args.sigma)
+    loader_train = DataLoader(dataset=dataset_train, batch_size=args.batchSize)
+    dataset_val = RootDataset(root_file=args.valfile, sigma=args.sigma)
+    val_train = DataLoader(dataset=dataset_val, batch_size=args.batchSize)
+
+    # Build model
+    model = DnCNN(channels=1, num_of_layers=args.num_of_layers, ker_size=args.kernelSize, o_k_size=args.outKerSize).to(device=args.device)
+    if (args.model == None):
+        model.apply(init_weights)
+        print("Creating new model")
+    else:
+        print("Loading model from file" + args.model)
+        model.load_state_dict(torch.load(args.model))
+        model.eval()
+
+    # Loss function
+    criterion = PatchLoss()
+    criterion.to(device=args.device)
+
+    #Optimizer
+    MyOptim = optim.Adam(model.parameters(), lr = args.lr)
+    MyScheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer=MyOptim, factor=0.1, patience=10, verbose=True)
+
+    # training and validation
+    step = 0
+    training_losses = np.zeros(args.epochs)
+    validation_losses = np.zeros(args.epochs)
+    for epoch in range(args.epochs):
+        print("Epoch #" + str(epoch))
+        # training
+        train_loss = 0
+        for i, data in enumerate(loader_train, 0):
+            model.train()
+            model.zero_grad()
+            MyOptim.zero_grad()
+            truth, noise = data
+            noise = noise.unsqueeze(1)
+            
+            o_kernel = model(noise.float().to(args.device))
+            
+            batch_loss = criterion(o_kernel.to(args.device), noise.to(args.device), truth.to(args.device), args.outKerSize, args.patchSize).to(args.device)
+            batch_loss.backward()
+            MyOptim.step()
+            model.eval()
+            train_loss += batch_loss.item()
+        training_losses[epoch] = train_loss
+        print("Training Loss: "+ str(train_loss))
+        
+        val_loss = 0
+        for i, data in enumerate(val_train, 0):
+            val_truth, val_noise =  data
+            v_kernel = model(val_noise.unsqueeze(1).float().to(args.device), args.outKerSize)
+            output_loss = criterion(v_kernel.to(args.device), val_noise.to(args.device), val_truth.to(args.device), args.outKerSize, args.patchSize).to(args.device)
+            val_loss+=output_loss.item()
+        MyScheduler.step(torch.tensor([val_loss]))
+        validation_losses[epoch] = val_loss
+        print("Validation Loss: "+ str(val_loss))
+        # save the model
+        model.eval()
+        torch.save(model.state_dict(), os.path.join(args.outf, 'net.pth'))
+    training = plt.plot(training_losses, label='Training')
+    validation = plt.plot(validation_losses, label='Validation')
+    plt.legend()
+    plt.savefig(args.outf + "/lossplt.png")
+    
+    branch = get_all_histograms("./test.root")
+    model.to('cpu')
+    for image in range(10):
+    
+        data = get_bin_weights(branch, image).copy()
+        np.savetxt(args.outf+'/truth#' + str(image) + '.txt', data)
+        
+        means = np.mean(data)
+        stdevs = np.std(data)
+        
+        noisy = add_noise(data, args.sigma).copy()
+        np.savetxt(args.outf+'/noisy#' + str(image) + '.txt', noisy)
+        
+        data_norm = (data-means)/stdevs
+        np.savetxt(args.outf+'/truth_norm#' + str(image) + '.txt', data_norm)
+        noisy_norm = (noisy-means)/stdevs
+        np.savetxt(args.outf+'/noisy_norm#' + str(image) + '.txt', noisy_norm)
+        
+        data_norm = torch.from_numpy(data_norm)
+        noisy_norm = torch.from_numpy(noisy_norm)
+        noisy_norm = noisy_norm.unsqueeze(0)
+        noisy_norm = noisy_norm.unsqueeze(1)
+        
+        norm_kernel = model(noisy_norm.float())
+        output_norm = compute_output(noisy_norm, norm_kernel, args.outKerSize).squeeze(0).squeeze(0).detach().numpy()
+        
+        #output_norm = model(noisy_norm.float(), args.outKerSize).squeeze(0).squeeze(0).detach().numpy()
+        np.savetxt(args.outf+'/output_norm#' + str(image) + '.txt', output_norm)
+        output = (output_norm * stdevs) + means
+        np.savetxt(args.outf+'/output#' + str(image) + '.txt', output)
+        truth = data.numpy()
+        noisy = noisy.numpy()
+        diff = output-truth
+        noisy_diff = noisy-truth
+        np.savetxt(args.outf+'/diff#' + str(image) + '.txt', diff)
+    model.to('cuda')
+    
+if __name__ == "__main__":
+    main()
+
