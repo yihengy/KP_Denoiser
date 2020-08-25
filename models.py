2e4a22015e692ab98079cf7b5fb716c0fa957ef4
yihengy <yihengy@uchicago.edu>
2020-08-25 15:44:58 +0800

    fix

--- models.py
+++ models.py
@@ -22,29 +22,40 @@ def calcOutput(data, kernel, ker_size=5):
     ZeroPad = nn.ZeroPad2d(padding=(pad, pad, pad, pad))
     data_pad = ZeroPad(data)
     reshape_data = data_pad.unfold(2,ker_size,1).unfold(3,ker_size,1)
-    
-    flatten_kernel = torch.flatten(kernel)
-    
     soft_max = nn.Softmax2d()
-    
-    reshape_kernel = flatten_kernel.reshape(N, in_ch, x, y, ker_size, ker_size)
+    reshape_kernel = kernel.reshape(N, in_ch, x, y, ker_size, ker_size)
+    print(reshape_kernel)
     for a in range(N):
         for b in range(in_ch):
-            soft_max(reshape_kernel[a][b])
+            reshape_kernel = soft_max(reshape_kernel[a][b])
+    print(reshape_kernel)
+    
     scalar_product = torch.mul(reshape_data, reshape_kernel)
+    result = torch.sum(scalar_product, dim=(4,5), keepdim=False)
+    return result
+
+def calcOutput_2(data, kernel, ker_size=5):
+    pad = (ker_size-1)//2
+    ZeroPad = nn.ZeroPad2d(padding=(pad, pad, pad, pad))
+    data_pad = ZeroPad(data)
     
-    result = torch.zeros(N, in_ch, x, y)
+    N = (list(data.size()))[0]
+    in_ch = (list(data.size()))[1]
+    x = (list(data.size()))[2]
+    y = (list(data.size()))[3]
+
+    pad = (ker_size-1)//2
+    ZeroPad = nn.ZeroPad2d(padding=(pad, pad, pad, pad))
+    data_pad = ZeroPad(data)
+    reshape_data = data_pad.unfold(2,ker_size,1).unfold(3,ker_size,1)
+    soft_max = nn.Softmax(dim=4)
+    reshape_kernel = kernel.reshape(N, in_ch, x, y, ker_size, ker_size)
+    #print(reshape_kernel)
+    reshape_kernel = soft_max(reshape_kernel[a][b])
+    print(reshape_kernel)
     
-    for o in range(N):
-        for p in range(in_ch):
-            for q in range(x):
-                for r in range(y):
-                    result[o][p][q][r] = torch.sum(scalar_product[o][p][q][r])
-    
-    print(result)
-    shit = torch.sum(scalar_product, dim=(4,5), keepdim=False)
-    print(shit)
-                    
+    scalar_product = torch.mul(reshape_data, reshape_kernel)
+    result = torch.sum(scalar_product, dim=(4,5), keepdim=False)
     return result
     
 class DnCNN(nn.Module):
@@ -100,9 +111,10 @@ if __name__=="__main__":
     test = torch.rand(10,1,5,5).unfold(2,3,1).unfold(3,3,1)
     print(test.size())
     
-    data = torch.rand(10, 1, 6, 6)
-    kernel = torch.rand(10, 9, 6, 6)
+    data = torch.rand(10, 1, 5, 5)
+    kernel = torch.rand(10, 9, 5, 5)
     result = calcOutput(data, kernel, 3)
+    result2 = calcOutput_2(data, kernel, 3)
     #print(result.size())
 
 
