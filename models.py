55d6cf222e9f42ff87469d48ea321d1b9c663763
yihengy <yihengy@uchicago.edu>
2020-08-26 20:56:09 +0800

    fix

--- models.py
+++ models.py
@@ -103,7 +103,7 @@ class PatchLoss(nn.Module):
         return avg_loss
         
 class NewPatchLoss(nn.Module):
-    def __init__(self, size_average=None, reduce=None, reduction: str = 'mean') -> None:
+    def __initII(self, size_average=None, reduce=None, reduction: str = 'mean') -> None:
         super(NewPatchLoss, self).__init__(size_average, reduce, reduction)
     
     def forward(self, output, target, patch_size):
