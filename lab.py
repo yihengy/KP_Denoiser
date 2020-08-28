5cfb4edc432c092034616edbe5783010fddb9bcc
yihengy <yihengy@uchicago.edu>
2020-08-28 10:51:10 +0800

    fix

---
+++ lab.py
@@ -0,0 +1,35 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as f
+from torch.autograd import Variable
+import numpy as np
+
+'''
+Given a batch of images and a HUGE tensor produced by the network,
+compute the output batch of images
+'''
+
+def calcOutput(data, kernel, ker_size=5):
+    pad = (ker_size-1)//2
+    ZeroPad = nn.ZeroPad2d(padding=(pad, pad, pad, pad))
+    data_pad = ZeroPad(data)
+    N = (list(data.size()))[0]
+    in_ch = (list(data.size()))[1]
+    x = (list(data.size()))[2]
+    y = (list(data.size()))[3]
+    pad = (ker_size-1)//2
+    ZeroPad = nn.ZeroPad2d(padding=(pad, pad, pad, pad))
+    data_pad = ZeroPad(data)
+    reshape_data = data_pad.unfold(2,ker_size,1).unfold(3,ker_size,1)
+    soft_max = nn.Softmax(dim=3)
+    reshape_kernel = kernel.reshape(N, in_ch, x, y, ker_size**2)
+    reshape_kernel = soft_max(reshape_kernel)
+    reshape_kernel = reshape_kernel.reshape(N, in_ch, x, y, ker_size, ker_size)
+    scalar_product = torch.mul(reshape_data, reshape_kernel)
+    result = torch.sum(scalar_product, dim=(4,5), keepdim=False)
+    return result
+    
+if __name__=="__main__":
+    arr = np.array([[[1, 2, 3, 4, 5],[1, 2, 3, 4, 5],[1, 2, 3, 4, 5],[1, 2, 3, 4, 5],[1, 2, 3, 4, 5]]])
+    tensor = torch.from_numpy(arr)
+    print(tensor.size())
